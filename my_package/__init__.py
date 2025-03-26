a9b4b31bad2e7518218c0e4477b923a2525465cb
Nicholas Duran <nicholas.duran@asu.edu>
2025-03-25 19:13:03 -0700

    updated init.py

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -7,8 +7,11 @@ from .bert_model import BertWrapper
 from .word2vec_model import Word2VecWrapper
 from .alignment_bert import SemanticAlignmentAnalyzer
 from .alignment_w2v import SemanticAlignmentW2V
+from .alignment_lexsyn import LexicalSyntacticAlignment
+from .alignment import SemanticAlignment
 
-__all__ = ['BertWrapper', 'Word2VecWrapper', 'SemanticAlignmentAnalyzer', 'SemanticAlignmentW2V']
+__all__ = ['BertWrapper', 'Word2VecWrapper', 'SemanticAlignmentAnalyzer', 
+           'SemanticAlignmentW2V', 'LexicalSyntacticAlignment', 'SemanticAlignment']
 
 # from .prepare_transcripts import *
 # from .calculate_alignment import *
